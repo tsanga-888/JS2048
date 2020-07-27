5dc724a626b3f2a8a3c49aa019d3a4aef248bf83
tsanga-888 <mradriannicolastsang@gmail.com>
2020-07-27 17:11:58 +1000

    Increase tile values.

--- app.js
+++ app.js
@@ -29,10 +29,10 @@ document.addEventListener("DOMContentLoaded", () => {
   // const playerActions = ["LEFT", "RIGHT", "UP", "DOWN"];
 
   board = [
-    [0, 2, 4, 8],
-    [16, 32, 64, 128],
-    [256, 512, 1024, 2048],
-    [4096, 8192, 16384, 0],
+    [2, 4, 8, 16],
+    [32, 64, 128, 256],
+    [512, 1024, 2048, 4096],
+    [8192, 16384, 32768, 65536],
   ];
   redrawBoard(board);
 
